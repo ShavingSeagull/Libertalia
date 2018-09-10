677ecc459e8536b541665a4782ae956d097528d5
Scott Kipp <scott.ecfx@gmail.com>
2018-09-10 23:07:04 +0100

    Finished 'About' section text, background and positioning

--- libertalia.py
+++ libertalia.py
@@ -9,9 +9,9 @@ display_width = 1200
 display_height = 700
 game_display = pygame.display.set_mode((display_width, display_height), pygame.RESIZABLE)
 
-intro_background = pygame.image.load('intro_background2.jpg')
-about_background = pygame.image.load('intro_background.jpg')
+intro_background = pygame.image.load('intro_background.jpg')
 button_background = pygame.image.load('button_background.png')
+about_background = pygame.image.load('about_background.png')
 
 # COLORS
 white = (255,255,255)
@@ -48,9 +48,40 @@ def button(msg, x, y, width, height, action=None):
     game_display.blit(img_surf, img_rect)
     game_display.blit(text_surf, text_rect)
 
+def blit_text(surface, text, pos, color=black):
+    screen_surf = game_display.get_rect()
+    small_text = pygame.font.Font('PirataOne-Regular.ttf', int(screen_surf[2] / 50))
+    words = [word.split(' ') for word in text.splitlines()]  # 2D array where each row is a list of words.
+    space = small_text.size(' ')[0]  # The width of a space.
+    max_width, max_height = surface.get_size()
+    max_width -= int(screen_surf[2] / 10 * 1)
+    x, y = pos
+    for line in words:
+        for word in line:
+            word_surface = small_text.render(word, 0, color)
+            word_width, word_height = word_surface.get_size()
+            if x + word_width >= max_width:
+                x = pos[0]  # Reset the x.
+                y += word_height  # Start on new row.
+            surface.blit(word_surface, (x, y))
+            x += word_width + space
+        x = pos[0]  # Reset the x.
+        y += word_height  # Start on new row
+
 def about():
     screen_surf = game_display.get_rect()
-    pygame.draw.rect(game_display, white, [screen_surf[2] / 85, screen_surf[3] / 4, screen_surf[2] / 10 * 7, screen_surf[3] / 10 * 7])
+    text = "Libertalia is a game inspired by the fabled location of Libertalia, Madagascar.\nSupposedly founded by " \
+           "Captain James Misson (although other reports cite Henry Avery), Libertalia functioned as a pirate utopia, " \
+           "a free man's land away from the oppressive monarchies of Europe. Other pirates are known to have founded " \
+           "colonies and cities, so it isn't a stretch of the imagination to think that they may have actually taken " \
+           "the opportunity to found their own country, too.\nAs a player, your goal is to trade and pillage your way " \
+           "across the Caribbean, amassing your fortune while staying out of the hands of the European authorities!"
+
+    img_surf = pygame.transform.scale(about_background, (int(screen_surf[2] / 10 * 7), int(screen_surf[3] / 10 * 7)))
+    img_rect = img_surf.get_rect()
+    blit_text(img_surf, text, (img_rect[2] / 10 * 1, img_rect[3] / 8 * 1))
+    game_display.blit(img_surf, (screen_surf[2] / 85, screen_surf[3] / 4))
+
 
 def quitgame():
     pygame.quit()
